f713515763b6ab4708b0d21049d30e42c6e4a747
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-26 11:33:33 +0700

    ss6 bai 3

---
+++ ss6 bai 3.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,a;
+	printf("Nhap mat khau :");
+	scanf("%d",&n);
+	
+	printf("kiem tra :\n");
+	scanf("%d",&a);
+	
+	if(n==a)
+	{
+		printf("chinh xac\n");
+	
+	}
+	else
+	{
+		printf("sai");
+	}
+	return 0;
+}
